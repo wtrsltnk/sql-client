c916239dd8576898a5e9754a39d4cc59b890b7b7
Wouter Saaltink <wouter.saaltink@gmail.com>
2019-09-08 17:18:13 +0200

    cleaned up odbc code te be more readable for me

--- odbcsql.cpp
+++ odbcsql.cpp
@@ -40,56 +40,54 @@ int OdbcMain(char const *connectionString) {
 	SQLCHAR outstr[1024];
 	SQLSMALLINT outstrlen;
 	int i = 0;
+
 	// Allocate environment handle
 	retcode = SQLAllocHandle(SQL_HANDLE_ENV, SQL_NULL_HANDLE, &henv);
-	CHECK_ERROR(retcode, "SQLAllocHandle(SQL_HANDLE_ENV)",
-		henv, SQL_HANDLE_ENV);
-	// Set the ODBC version environment attribute
-	retcode = SQLSetEnvAttr(henv, SQL_ATTR_ODBC_VERSION,
-		(SQLPOINTER*)SQL_OV_ODBC3, 0);
-	CHECK_ERROR(retcode, "SQLSetEnvAttr(SQL_ATTR_ODBC_VERSION)",
-		henv, SQL_HANDLE_ENV);
+	CHECK_ERROR(retcode, "SQLAllocHandle(SQL_HANDLE_ENV)", henv, SQL_HANDLE_ENV);
 
-	retcode = SQLSetEnvAttr(henv, SQL_ATTR_ODBC_VERSION,
-		(SQLPOINTER*)SQL_OV_ODBC3, 0);
-	CHECK_ERROR(retcode, "SQLSetEnvAttr(SQL_ATTR_ODBC_VERSION)",
-		henv, SQL_HANDLE_ENV);
+	// Set the ODBC version environment attribute
+	retcode = SQLSetEnvAttr(henv, SQL_ATTR_ODBC_VERSION, (SQLPOINTER*)SQL_OV_ODBC3, 0);
+	CHECK_ERROR(retcode, "SQLSetEnvAttr(SQL_ATTR_ODBC_VERSION)", henv, SQL_HANDLE_ENV);
+
+	retcode = SQLSetEnvAttr(henv, SQL_ATTR_ODBC_VERSION, (SQLPOINTER*)SQL_OV_ODBC3, 0);
+	CHECK_ERROR(retcode, "SQLSetEnvAttr(SQL_ATTR_ODBC_VERSION)", henv, SQL_HANDLE_ENV);
+
 	// Allocate connection handle
 	retcode = SQLAllocHandle(SQL_HANDLE_DBC, henv, &hdbc);
-	CHECK_ERROR(retcode, "SQLAllocHandle(SQL_HANDLE_DBC)",
-		hdbc, SQL_HANDLE_DBC);
+	CHECK_ERROR(retcode, "SQLAllocHandle(SQL_HANDLE_DBC)", hdbc, SQL_HANDLE_DBC);
+
 	// Set login timeout to 5 seconds
 	SQLSetConnectAttr(hdbc, SQL_LOGIN_TIMEOUT, (SQLPOINTER)5, 0);
-	CHECK_ERROR(retcode, "SQLSetConnectAttr(SQL_LOGIN_TIMEOUT)",
-		hdbc, SQL_HANDLE_DBC);
+	CHECK_ERROR(retcode, "SQLSetConnectAttr(SQL_LOGIN_TIMEOUT)", hdbc, SQL_HANDLE_DBC);
+
 	// Connect to data source, replace with your connection string
 	retcode = SQLDriverConnect(hdbc, NULL, (SQLCHAR *)connectionString, SQL_NTS, outstr, sizeof(outstr), &outstrlen, SQL_DRIVER_NOPROMPT);
+
 	// Allocate statement handle
 	retcode = SQLAllocHandle(SQL_HANDLE_STMT, hdbc, &hstmt);
-	CHECK_ERROR(retcode, "SQLAllocHandle(SQL_HANDLE_STMT)",
-		hstmt, SQL_HANDLE_STMT);
-	retcode = SQLExecDirect(hstmt,
-		(SQLCHAR*) "SELECT DB_NAME()", SQL_NTS);
+	CHECK_ERROR(retcode, "SQLAllocHandle(SQL_HANDLE_STMT)", hstmt, SQL_HANDLE_STMT);
+
+	retcode = SQLExecDirect(hstmt, (SQLCHAR*) "SELECT DB_NAME()", SQL_NTS);
 	CHECK_ERROR(retcode, "SQLExecDirect()", hstmt, SQL_HANDLE_STMT);
 
 	// Bind columns
-
 	retcode = SQLBindCol(hstmt, 1, SQL_C_CHAR, &strResult, RESULT_LEN, 0);
 
-
 	// Fetch and print each row of data until SQL_NO_DATA returned.
-	for (i = 0; ; i++) {
+	for (i = 0; ; i++)
+	{
 		retcode = SQLFetch(hstmt);
-		if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
+		if (retcode == SQL_NO_DATA)
+		{
+			break;
+		}
+		if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO)
+		{
 			printf("Result is: %s", strResult);
 		}
-		else {
-			if (retcode != SQL_NO_DATA) {
-				CHECK_ERROR(retcode, "SQLFetch()", hstmt, SQL_HANDLE_STMT);
-			}
-			else {
-				break;
-			}
+		else
+		{
+			CHECK_ERROR(retcode, "SQLFetch()", hstmt, SQL_HANDLE_STMT);
 		}
 	}
 
@@ -98,14 +96,22 @@ exit:
 	// Free handles
 	// Statement
 	if (hstmt != SQL_NULL_HSTMT)
+	{
 		SQLFreeHandle(SQL_HANDLE_STMT, hstmt);
+	}
+
 	// Connection
-	if (hdbc != SQL_NULL_HDBC) {
+	if (hdbc != SQL_NULL_HDBC)
+	{
 		SQLDisconnect(hdbc);
 		SQLFreeHandle(SQL_HANDLE_DBC, hdbc);
 	}
+
 	// Environment
 	if (henv != SQL_NULL_HENV)
+	{
 		SQLFreeHandle(SQL_HANDLE_ENV, henv);
+	}
+
 	return 0;
 }
